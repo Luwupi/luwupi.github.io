b8163db9e459c6f0d4049b48e85bcf207d0bb18a
Luwupi <92521982+Luwupi@users.noreply.github.com>
2023-09-23 01:24:18 +1200

    Fixed syntax error

--- test/main.js
+++ test/main.js
@@ -1,7 +1,7 @@
 import * as ping from "./cmd/ping.js";
 
 export function processCmd(msg) {
-	c = msg.split(/\s+/g, 1)[0]; //split on whitespace
+	let c = msg.split(/\s+/g, 1)[0]; //split on whitespace
 	switch (c) {
 		case "ping":
 			return ping.run(msg);
